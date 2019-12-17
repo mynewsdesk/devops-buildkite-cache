07eaec136e536b4d3316b0de27d249b311f27ccb
David Backeus <duztdruid@gmail.com>
2019-12-17 13:02:51 +0100

    Allow disabling the plugin via ENV var

--- lib/buildkite-cache.rb
+++ lib/buildkite-cache.rb
@@ -8,14 +8,14 @@ module BuildkiteCache
   def self.generate_configuration(json_string = nil)
     keys_and_paths = json_string ? parse_configuration(json_string) : {}
 
-    if File.exist?("Gemfile.lock")
+    if File.exist?("Gemfile.lock") && ENV["BUILDKITE_CACHE_DISABLE_RUBY"] != "true"
       ruby_version = language_version(".ruby-version")
       checksum = checksum("Gemfile.lock")
       key = key("bundle-#{ruby_version}-#{checksum}")
       keys_and_paths[key] = "vendor/bundle"
     end
 
-    if File.exist?("yarn.lock")
+    if File.exist?("yarn.lock") && ENV["BUILDKITE_CACHE_DISABLE_NODE"] != "true"
       node_version = language_version(".node-version", ".nvmrc")
       checksum = checksum("yarn.lock")
       key = key("node_modules-#{node_version}-#{checksum}")
